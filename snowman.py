7c2807ba1151a8dded78df606932d38c50793767
Eric Barwinsky <ericbarwinsky@googlemail.com>
2026-03-13 10:25:22 +0100

    Added display function to show ASCII art and hidden or guessed letters.

--- snowman.py
+++ snowman.py
@@ -41,18 +41,31 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    display_word = []
+    for char in secret_word:
+        if char in guessed_letters:
+            display_word.append(char)
+        else:
+            display_word.append("_")
+    print(f"Secret word selected: " + " ".join(display_word))
+
+
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
     mistakes = 0
+    guessed_letters = []
     while mistakes != 3:
+        display_game_state(mistakes, secret_word, guessed_letters)
         guess = input("Guess a letter: ").lower()
         print("You guessed:", guess)
         if guess not in secret_word:
             mistakes += 1
-
+        else:
+            guessed_letters.append(guess)
 
 if __name__ == "__main__":
     play_game()
