0e3a006f59d0aaaa42727e1d03c94106639a8a83
Eric Barwinsky <ericbarwinsky@googlemail.com>
2026-03-13 11:24:03 +0100

    All functions and game logic implemented and tested. Ready for refactoring.

--- snowman.py
+++ snowman.py
@@ -52,20 +52,41 @@ def display_game_state(mistakes, secret_word, guessed_letters):
     print(f"Secret word selected: " + " ".join(display_word))
 
 
+def right_word(secret_word, guessed_letters):
+    letters_secret_word = set(secret_word)
+    set_guessed_letters = set(guessed_letters)
+    all_guessed = letters_secret_word.issubset(set_guessed_letters)
+    return all_guessed
+
+
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
 
     mistakes = 0
     guessed_letters = []
+    letter_game_list =[]
+
     while mistakes != 3:
         display_game_state(mistakes, secret_word, guessed_letters)
         guess = input("Guess a letter: ").lower()
         print("You guessed:", guess)
-        if guess not in secret_word:
+        if guess in letter_game_list:
+            print("You already guessed that letter.")
+        elif guess not in secret_word:
+            letter_game_list.append(guess)
             mistakes += 1
         else:
             guessed_letters.append(guess)
+            letter_game_list.append(guess)
+
+        if right_word(secret_word, guessed_letters):
+            print("Congratulation! you have safed the snowman!")
+            break
+
+    if mistakes == 3:
+        print(f"Game Over! The word was: {secret_word}")
+
 
 if __name__ == "__main__":
     play_game()
